f2c4c301ade8efb785546b0209655a0156526b91
ckyeow0922 <165008086+ckyeow0922@users.noreply.github.com>
2025-03-04 19:51:36 +0800

    Add project files.

---
+++ RentPick/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentPick.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
